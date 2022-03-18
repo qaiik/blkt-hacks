f71f24f162af1d71254009c3a13cdd5b5c5a1a08
qaiik <82181231+qaiik@users.noreply.github.com>
2022-03-17 21:15:30 -0600

    Update setdata-pull.js

--- setdata-pull.js
+++ setdata-pull.js
@@ -3,10 +3,11 @@ let gdata = {};
     const origOpen = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function() {
         if (arguments[1].startsWith("https://api.blooket.com/api/games?gameId=")) {
-          
-        this.addEventListener('load', function() {
-            console.log(JSON.parse(this.responseText));
-        });
+            this.addEventListener('load', function() {
+                console.log(JSON.parse(this.responseText));
+            });   
+        }
+        
         origOpen.apply(this, arguments);
     };
 }))();
